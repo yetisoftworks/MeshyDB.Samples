2215f781abce5de7070b88fe36a75bd82cf12431
rgails <rgails@users.noreply.github.com>
2019-08-26 18:43:14 -0500

    Update MockDataStore.cs

--- Xamarin/Blog/MeshyForums/MeshyForums/Services/MockDataStore.cs
+++ Xamarin/Blog/MeshyForums/MeshyForums/Services/MockDataStore.cs
@@ -15,12 +15,12 @@ namespace MeshyForums.Services
             items = new List<Item>();
             var mockItems = new List<Item>
             {
-                //new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                //new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                //new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                //new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                //new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                //new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                new Item { /*Id = Guid.NewGuid().ToString(),*/ Text = "First item", Description="This is an item description." },
+                new Item { /*Id = Guid.NewGuid().ToString(),*/ Text = "Second item", Description="This is an item description." },
+                new Item { /*Id = Guid.NewGuid().ToString(),*/ Text = "Third item", Description="This is an item description." },
+                new Item { /*Id = Guid.NewGuid().ToString(),*/ Text = "Fourth item", Description="This is an item description." },
+                new Item { /*Id = Guid.NewGuid().ToString(),*/ Text = "Fifth item", Description="This is an item description." },
+                new Item { /*Id = Guid.NewGuid().ToString(),*/ Text = "Sixth item", Description="This is an item description." }
             };
 
             foreach (var item in mockItems)
